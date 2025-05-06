fdb7350c3ce1bef1cdfb1b1fc177db72732dbab4
mdeandwibekti <mdeandwibekti@gmail.com>
2025-05-06 11:00:49 +0700

    membuat void addnode

--- singlelinked_list.cpp
+++ singlelinked_list.cpp
@@ -14,6 +14,31 @@ class LinkedList {
         LinkedList() {
             START = NULL;
         }
+
+        void addNode()
+        {
+            int nim;
+            cout << "\nMasukkan Nomor Mahasiswa: ";
+            cin >> nim;
+
+            Node * newNode = new Node;
+            newNode->noMhs = nim;
+
+            if (START == NULL || nim <= START->noMhs) 
+            {
+                if ((START != NULL) && (nim == START->noMhs)) {
+                    cout << "\nDuplikat noMhs tidak diijinkan\n" ;
+                    return;
+                }
+                newNode->next = START;
+                START = newNode;
+                return;
+            }   
+        }
+
+        
+
+                
         
         
 
